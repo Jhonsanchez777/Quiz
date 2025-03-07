ed4d4babc3efdb406429295452db9977dca7ecb7
Jhon <Jhonsan806@gmail.com>
2025-03-07 13:45:14 -0500

    se añade el quiz

---
+++ src/EjerciconUno.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EjerciconUno {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese el primer numero: ");
+        int numero = scanner.nextInt();
+        
+    }
+}
